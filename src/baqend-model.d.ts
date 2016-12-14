780849ac75b796bba46745669f7eaaf366783b31
Hannes <hk@baqend.com>
2016-12-14 01:24:29 +0100

    added baqend typings

---
+++ src/baqend-model.d.ts
@@ -0,0 +1,31 @@
+import {binding, GeoPoint} from "baqend";
+
+declare module "baqend" {
+
+  interface baqend {
+    Message: binding.EntityFactory<model.Message>;
+  }
+
+  namespace model {
+    interface Device extends binding.Entity {
+      deviceOs: string;
+    }
+
+    interface Role extends binding.Entity {
+      name: string;
+      users: Set<User>;
+    }
+
+    interface User extends binding.Entity {
+      username: string;
+      inactive: boolean;
+    }
+
+    interface Message extends binding.Entity {
+      name: string;
+      text: string;
+      face: string;
+    }
+
+  }
+}
